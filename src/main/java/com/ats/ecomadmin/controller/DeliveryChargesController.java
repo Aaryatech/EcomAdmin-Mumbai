648585de61207c3496f1220b5e6cedb3647b8f2a
maddy <maddy@maddy-Inspiron-3541>
2020-12-31 10:33:55 +0530

    //maddy

--- src/main/java/com/ats/ecomadmin/controller/DeliveryChargesController.java
+++ src/main/java/com/ats/ecomadmin/controller/DeliveryChargesController.java
@@ -3,6 +3,8 @@ package com.ats.ecomadmin.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,16 +12,20 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ats.ecomadmin.commons.AccessControll;
 import com.ats.ecomadmin.commons.Constants;
 import com.ats.ecomadmin.commons.FormValidation;
+import com.ats.ecomadmin.model.CompMaster;
 import com.ats.ecomadmin.model.DeliveryCharges;
+import com.ats.ecomadmin.model.ExportToExcel;
 import com.ats.ecomadmin.model.Info;
 import com.ats.ecomadmin.model.User;
 import com.ats.ecomadmin.model.acrights.ModuleJson;
@@ -31,11 +37,12 @@ public class DeliveryChargesController {
 	
 	
 		// Delivery Charges
+		List<DeliveryCharges> chargeList = new ArrayList<DeliveryCharges>();
 		@RequestMapping(value = "/showDeliveryChargesList", method = RequestMethod.GET)
 		public ModelAndView showDeliveryChargesList(HttpServletRequest request, HttpServletResponse response) {
 
 			ModelAndView model = null;
-			List<DeliveryCharges> chargeList = new ArrayList<DeliveryCharges>();
+		
 			try {
 				
 				HttpSession session = request.getSession();
@@ -66,6 +73,49 @@ public class DeliveryChargesController {
 						chargeList.get(i).setExVar1(FormValidation.Encrypt(String.valueOf(chargeList.get(i).getChId())));
 					}
 					model.addObject("chargeList", chargeList);
+					
+					List<ExportToExcel> exportToExcelList = new ArrayList<ExportToExcel>();
+
+					ExportToExcel expoExcel = new ExportToExcel();
+					List<String> rowData = new ArrayList<String>();
+
+						rowData.add("Sr No");						
+						rowData.add("Group Name");						
+						rowData.add("MIN Km.");						
+						rowData.add("MAX Km.");						
+						rowData.add("Amt1");						
+						rowData.add("Amt2");
+					
+					expoExcel.setRowData(rowData);
+					
+					exportToExcelList.add(expoExcel);
+					int srno = 1;
+					for (int i = 0; i < chargeList.size(); i++) {
+						expoExcel = new ExportToExcel();
+						rowData = new ArrayList<String>();
+						rowData.add(" "+srno);						
+						rowData.add(" " + chargeList.get(i).getGroupName());
+						rowData.add(" " + chargeList.get(i).getMinKm());
+						rowData.add(" " + chargeList.get(i).getMaxKm());
+						rowData.add(" " + chargeList.get(i).getAmt1());
+						rowData.add(" " + chargeList.get(i).getAmt2());
+				
+						srno = srno + 1;
+						
+						expoExcel.setRowData(rowData);
+						exportToExcelList.add(expoExcel);
+
+					}
+					session.setAttribute("exportExcelListNew", exportToExcelList);
+					session.setAttribute("excelNameNew", "DeliverChargesList");
+					session.setAttribute("reportNameNew", "Deliver Charges List");
+					session.setAttribute("searchByNew", "All");
+					session.setAttribute("mergeUpto1", "$A$1:$L$1");
+					session.setAttribute("mergeUpto2", "$A$2:$L$2");
+
+					session.setAttribute("exportExcelList", exportToExcelList);
+					session.setAttribute("excelName", "Deliver Charges List Excel");
+					
 				}
 				
 			} catch (Exception e) {
@@ -74,6 +124,22 @@ public class DeliveryChargesController {
 			}
 
 			return model;
+		}
+		
+		@RequestMapping(value = "pdf/getDelvrChrgListPdf", method = RequestMethod.GET)
+		public String getProductListPdf(HttpServletRequest request,
+				HttpServletResponse response, Model model) {
+			try {
+				HttpSession session = request.getSession();
+				CompMaster company = (CompMaster) session.getAttribute("company");
+				
+					model.addAttribute("chargeList", chargeList);
+					model.addAttribute("company", company.getCompanyName());
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return "pdfs/devrChrgListPdf";
+			
 		}
 		
 		
--- src/main/webapp/WEB-INF/views/delvrChargList.jsp
+++ src/main/webapp/WEB-INF/views/delvrChargList.jsp
@@ -114,6 +114,15 @@
 								onclick="deletSelctd()">
 								Delete <i class="far fa-trash-alt"></i>
 							</button>
+							
+							<button type="button" class="btn btn-primary" id="submtbtn"
+								onclick="exportToExcel()">
+								Excel <i class="far fa-file-excel"></i>
+							</button> 
+
+							<button type="button" class="btn btn-primary" id="submtbtn1"  onclick="genPdf()">
+								Pdf<i class="fas fa-file-pdf"></i>
+							</button>
 							</div>
 					</div>
 				</div>
@@ -304,6 +313,15 @@
 		}//end of if !isError
 	}
 
+	function exportToExcel() {
+		window.open("${pageContext.request.contextPath}/exportToExcelNew");
+		document.getElementById("expExcel").disabled = true;
+	}
+
+	function genPdf() {
+		window
+				.open("${pageContext.request.contextPath}/pdfReport?url=pdf/getDelvrChrgListPdf");
+	}
 	</script>
 </body>
 </html>
